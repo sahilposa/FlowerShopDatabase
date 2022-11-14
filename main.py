2a5d3f94cad9051111e4375e336b3215c3f059f3
JSumlin <104284083+JSumlin@users.noreply.github.com>
2022-11-13 23:05:58 -0500

    functioning place-order page + cleanup

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 # Joseph Sumlin, Andy Kincheloe, Sahil Posa
-def get_db_connection():
-    conn = sqlite3.connect('flowershopdatabase.db', check_same_thread=False)
-    conn.row_factory = sqlite3.Row
-    return conn
 
 import sqlite3
 from database import Database
 from schema import Schema
 from flask import Flask, render_template, request, url_for, flash, redirect
+import os
 
 app = Flask(__name__)
-from database import Database
+app.config["SECRET_KEY"] = str(os.urandom(24).hex())
 
 conn = sqlite3.connect('flowershopdatabase.db', check_same_thread=False)
 conn.execute("PRAGMA foreign_keys = 1")
 cursor = conn.cursor()
 db = Database(conn,cursor)
 Schema.build(conn, cursor)
-db.add_cus('Andy','Not',9999999999)
 conn.close()
 
 
@@ -46,16 +42,25 @@ def customers():
 @app.route("/place-order", methods=("GET", "POST"))
 def place_ord():
     conn = get_db_connection()
+    cursor = conn.cursor()
     products = conn.execute("SELECT * FROM product")
+    id = 1
+    list = []
     if request.method == "POST":
-        print("in post")
-        roses = request.form['1']
-        flower2 = request.form['2']
-        flower3 = request.form['3']
-        flower4 = request.form['4']
-        flower5 = request.form['5']
-        print(roses)
-        redirect(url_for('index'))
+        phone = request.form['phone']
+        if not phone:
+            phone = None
+        elif cursor.execute("SELECT * FROM customer WHERE phone=?", (int(phone),)).fetchone() is None:
+            flash("Phone not found")
+            return render_template('placeOrder.html', products=products)
+        else:
+            phone=int(phone)
+        for x in conn.execute("SELECT productID FROM product"):
+            list.append(request.form[''+str(id)])
+            id = id+1
+        db = Database(conn, cursor)
+        db.ord_transaction(phone, list)
+        return redirect(url_for('index'))
     return render_template('placeOrder.html', products=products)
 
     
